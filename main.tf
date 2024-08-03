70a7dde018ab768a5ff507c727ed9394100ed4b0
omolaraadisa <adisaomolara.oa@gmail.com>
2024-08-03 14:49:54 +0100

    Modify ip configuration name

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_interface" "demo_nic" {
   resource_group_name = azurerm_resource_group.demo_rg.name
 
   ip_configuration {
-    name                          = "lara_ip-conf"
+    name                          = "lara_ip_conf"
     subnet_id                     = azurerm_subnet.demo_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.demo_ip.id
