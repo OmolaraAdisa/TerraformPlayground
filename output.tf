634f0e406203bbb8ad19f486c18d03b0016d1d2b
omolaraadisa <adisaomolara.oa@gmail.com>
2024-08-10 10:56:58 +0100

    Create output file

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "virtual_network_name" {
+  description = "Virtual Network Name"
+  value = "azurerm_virtual_network.demo_vnet"
+}
+
+output "virtual_network_id" {
+  description = "Virtual Network ID"
+  value = "azurerm_virtual_network.demo_vnet.id"
+}
