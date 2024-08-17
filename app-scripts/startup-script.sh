ef58a3039c735e6825f0a6419a3ee398989fab67
omolaraadisa <adisaomolara.oa@gmail.com>
2024-08-17 20:13:06 +0100

    Fix custom data error

--- app-scripts/startup-script.sh
+++ app-scripts/startup-script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#sudo yum update -y
+sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
-sudo systemcl start httpd
-sudo systemcl stop firewalld
-sudo systemcl disable firewalld
+sudo systemctl start httpd
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
 sudo chmod -R 777 /var/www/html
 sudo echo "Welcome to your web server - VM Hostname: $(hostname)" > /var/www/html/index.html
 sudo mkdir /var/www/html/app1
