39399b856d8374e2138af269b04eef8ce8abbf43
omolaraadisa <adisaomolara.oa@gmail.com>
2024-08-17 16:39:39 +0100

    Add virtual machine custom data and vm outputs

--- output.tf
+++ output.tf
@@ -7,3 +7,18 @@ output "virtual_network_id" {
   description = "Virtual Network ID"
   value       = "azurerm_virtual_network.demo_vnet.id"
 }
+
+output "web_linuxvm_public_ip" {
+  description = "Web Linux virtual machine public ip"
+  value = azurerm_public_ip.web_linuxvm_public_ip.ip_address
+}
+
+output "web_linuxvm_nic" {
+  description = "Web Linux virtual machine Network Interface Card"
+  value = azurerm_network_interface.web_linuxvm_nic.id
+}
+
+output "web_linuxvm_privateip" {
+  description = "Web Linux virtual machine Network Private IP"
+  value = [azurerm_network_interface.web_linuxvm_nic.private_ip_address]
+}
