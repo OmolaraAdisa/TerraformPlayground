52b505236c700986d32d559417cab59fccb1c371
omolaraadisa <adisaomolara.oa@gmail.com>
2024-08-31 22:59:48 +0100

    Add bastion host linux vm

--- networking.tf
+++ networking.tf
@@ -207,7 +207,7 @@ resource "azurerm_network_interface" "bastion_host_linuxvm_nic" {
   resource_group_name = azurerm_resource_group.demo_rg.name
 
   ip_configuration {
-    name                          = "bastion-vmip-conf"
+    name                          = "bastion-host-ip-1"
     subnet_id                     = azurerm_subnet.bastion_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.bastion_host_public_ip.id
